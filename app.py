41e658b239ecc3eb062e3ee99ecc3bb6a18b15a4
akashbhari-25 <akashbhari@gmail.com>
2026-05-17 16:19:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,49 @@ st.set_page_config(
 st.title("Institutional Quant Platform")
 st.sidebar.header("Quant Controls")
 
+# =========================
+# KPI DASHBOARD
+# =========================
+
+portfolio_return = (
+    returns.mean().mean() * 252
+)
+
+portfolio_volatility = (
+    returns.std().mean() * np.sqrt(252)
+)
+
+portfolio_sharpe = (
+    portfolio_return /
+    portfolio_volatility
+)
+
+portfolio_drawdown = (
+    drawdown.min().mean()
+)
+
+col1, col2, col3, col4 = st.columns(4)
+
+col1.metric(
+    "Expected Return",
+    f"{portfolio_return:.2%}"
+)
+
+col2.metric(
+    "Portfolio Volatility",
+    f"{portfolio_volatility:.2%}"
+)
+
+col3.metric(
+    "Sharpe Ratio",
+    f"{portfolio_sharpe:.2f}"
+)
+
+col4.metric(
+    "Max Drawdown",
+    f"{portfolio_drawdown:.2%}"
+)
+
 selected_tickers = st.sidebar.multiselect(
     "Select Stocks",
     [
