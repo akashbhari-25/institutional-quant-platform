abeeb35436234be71d58b90b1e00701a266fabec
akashbhari-25 <akashbhari@gmail.com>
2026-05-17 12:53:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import yfinance as yf
+import plotly.express as px
 
 st.set_page_config(
     page_title="Institutional Quant Platform",
